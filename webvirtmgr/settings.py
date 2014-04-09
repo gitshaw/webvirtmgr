fba81d3174bcddc121a06537a5b281bae98495ac
gitshaw <root@hv1.leedshost.co.uk>
2014-04-09 09:36:53 +0100

    Enable debug and ssl mode

--- webvirtmgr/settings.py
+++ webvirtmgr/settings.py
@@ -1,8 +1,10 @@
 # Django settings for webvirtmgr project.
 import os
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -64,7 +66,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', 'static')
+#STATIC_ROOT = os.path.join(os.path.dirname(__file__), '..', 'static')
+
+STATIC_ROOT = os.path.join(os.path.dirname(
+                os.path.abspath(__file__)), 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
